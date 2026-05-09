84482ec1c0eb41c677a441c19d14a0df8dc8c8b1
ButerBrod <volknekto2@gmail.com>
2026-05-09 18:57:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'comics-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    'https://wolf111234.github.io/ReadMyComics/'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});

a18a9298609d5493e1c2b6c50514b0c7ce6c2eae
ButerBrod <volknekto2@gmail.com>
2026-05-15 14:45:46 +0300

    Delete peer-server directory

--- peer-server/index.js
+++
@@ -1,7 +0,0 @@
-const { PeerServer } = require('peer');
-
-const PORT = process.env.PORT || 10000;
-
-PeerServer({ port: PORT, path: '/peerjs' }, () => {
-  console.log('PeerJS server on port ' + PORT);
-});
